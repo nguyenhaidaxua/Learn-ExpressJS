f7dc97f419227f9ba5e934a170a67bdea163f344
Nguyen Hai <nguyenhaidaxua@gmail.com>
2021-12-13 15:10:39 +0700

    add db

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const { render } = require('pug')
 const app = express()
 const bodyParser = require('body-parser') 
-
+const low = require('lowdb')
+const shortid = require('shortid')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+db = low(adapter)
+// Set some defaults
+db.defaults({ users: [] })
+    .write()
 const port = 3000
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -14,22 +21,18 @@ app.get('/', (req,res) => {
     res.render('index')
 })
 
-const users = [
-    { id: 1, name: 'Hai' },
-    { id: 2, name: 'Hao' },
-    { id: 3, name: 'Giang' },
-    { id: 4, name: 'Phat' }
-]
+
 
 app.get('/users', (req, res) => {
     res.render('user/index', {
-        users: users
+        users: db.get('users').value()
         
     })
 })
 app.get('/users/search', (req,res) => {
     const q = req.query.q 
-    const matchUser = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+    const matchUser = db.get('users').value()
+    .filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1)
     
     res.render('user/index', {
         users: matchUser,
@@ -40,8 +43,18 @@ app.get('/users/create', (req,res) => {
     res.render('user/createUser') 
 })
 
+app.get('/users/:id', (req,res) => {
+    const id = req.params.id
+
+    const user = db.get('users').find({id : id}).value()
+    res.render('user/view', {
+        user: user
+    })
+})
+
 app.post('/users/create', (req,res) => {
-    users.push(req.body)
+    req.body.id = shortid.generate()
+    db.get('users').push(req.body).write()
     res.redirect('/users')
 })
 app.listen(3000, ()=>{
